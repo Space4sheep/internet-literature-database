4e1c6952f60b586ef6b24db188f88ca4a6729d3d
Vladyslav <space4sheep@gmail.com>
2023-02-07 15:35:26 +0200

    Add logout and register form to users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    if request.method != 'POST':
+        # Показати порожню форму.
+        form = UserCreationForm()
+    else:
+        # Опрацювати заповнену форму.
+        form = UserCreationForm(data=request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            # Аворизувати користувача та скерувати його на головну сторінку.
+            login(request, new_user)
+            return redirect('main:home')
+
+    # Показати порожню або недійсну форму.
+    return render(request, 'registration/register.html', {'form': form})
+
