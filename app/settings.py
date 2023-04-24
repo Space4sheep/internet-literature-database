a0da3e3e59eff154d7dcd10c21a8dd38f8e64aa0
Space4sheep <118350533+Space4sheep@users.noreply.github.com>
2023-04-24 12:19:46 +0300

    [BagFix] Secret key is hidden

--- app/settings.py
+++ app/settings.py
@@ -1,4 +1,3 @@
-from local_data.django import DJANGO_SECRET_KEY
 """
 Django settings for app project.
 
@@ -21,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = DJANGO_SECRET_KEY
+SECRET_KEY = 'DJANGO_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
